4d5d0945c1793f80c9142991f9ac2b5eb43fc677
kathleenmcc <kathleenmcc@gmail.com>
2022-04-02 12:49:10 -0400

    displays browser

--- ConnectFour/js/app.js
+++ ConnectFour/js/app.js
@@ -1,8 +1,8 @@
 /*----- constants -----*/
 const lookup = {
-    playerBlue: 'blue',
+    playerB: 'blue',
 
-    playerYellow: 'yellow'
+    playerY: 'yellow'
 }
 
 /*----- app's state (variables) -----*/
@@ -10,6 +10,51 @@ let board, turn, winner;
 
 /*----- cached element references -----*/
 
+const slotEls = document.querySelectorAll('div.slot');
+const message = document.querySelector('h1');
+
 /*----- event listeners -----*/
 
+document.querySelector('div.slot').addEventListener('click', handleMove);
+document.querySelector('button').addEventListener('click', initialize);
+
 /*----- functions -----*/
+
+initialize();
+
+function handleMove(evt) {
+
+const slotChoice = parseInt(evt.target.id.replace('choice', ''));
+
+//do I have to create a 'changeColor' 
+
+//I think 'slotChoice' will correspond with id number of each slot?
+//if all squares have been clicked && no winner?
+if(board (|| winner) return; 
+// update state (board, turn, winner)
+winner = getWinner(); 
+render();
+
+
+function getWinner() {
+    
+}
+
+function render() {
+    slots.forEach(function(el)
+            slotEls.forEach(el => el.style.backgroundColor = 'white';//or 'null'
+
+    
+    //need 'if' statement to determine winner
+    if (/*(all slots 'true'? && winner !== )*/) {
+
+        message.innerHTML = 'No winner. It/s a draw';
+
+    } else if (winner) {
+        message.innerHTML = `${lookup[winner].toUpperCase()} wins!`;
+
+    function initialize() {
+    
+        turn = playerBlue;
+        render();
+    }

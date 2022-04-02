a520f624db6969da475d0ddf1fa4d38e9c5d74d3
kathleenmcc <kathleenmcc@gmail.com>
2022-04-02 02:25:16 -0400

    add slots and grid

---
+++ ConnectFour/js/app.js
@@ -0,0 +1,15 @@
+/*----- constants -----*/
+const lookup = {
+    playerBlue: 'blue',
+
+    playerYellow: 'yellow'
+}
+
+/*----- app's state (variables) -----*/
+let board, turn, winner;
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/

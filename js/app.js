e097ec6cca75a858fa6be936739607f271503c36
kathleenmcc <kathleenmcc@gmail.com>
2022-04-05 18:10:07 -0400

    changes to winner code

--- js/app.js
+++ js/app.js
@@ -28,23 +28,25 @@ function handleMove(evt) {
     render();
     turn = turn * -1;
 }
-function getWinner() {
+//below was changed 
+function winner(idx, inc) {
     let checkIdx = idx;
-    let row = 0;
-    while (gameboard[checkIdx] === turn) {
-        row++
-        checkIdx++;
+    let theRow = 0;
+    while (gameBoard[checkIdx] === turn && checkIdx < gameBoard.length){
+        theRow++; 
+        checkIdx = checkIdx + inc;
     }
-    checkIdx = idx -1;
-}
-while (gameboard[checkIdx] === turn) {
-    row++
-    checkIdx--;
-}
-if (row >= 4) {
-    result = turn;
-};
+    checkIdx = idx - 1;
 
+    while (gameBoard[checkIdx] === turn && checkIdx >= 0){
+        theRow++; 
+        checkIdx = checkIdx - inc
+
+    }
+    if (theRow >= 4) {
+        result = turn;
+    }; 
+}
 
 function render() {
     slotEls.forEach(el => el.style.backgroundColor = 'burlywood');
@@ -71,3 +73,17 @@ function renderBoard() {
         slotEls[idx].style.backgroundColor = lookup[slot];
     });
 }
+  
+  function render() {
+    circleEls.forEach(function(circleEl, idx) {
+      circleEl.style.backgroundColor = gameBoard[idx];
+    });
+   if (result) {msgEl.innerText = `${result} WINS!`;}
+   else {
+       msgEl.innerText = '';
+   }
+  }
+   winner(idx, 1);
+    winner(idx, 6);
+    winner(idx, 5);
+    winner(idx, 7);
